50d83798afd32e3fd97bf61fc9ff79110dd7321a
Carlos Muñoz <carlos.munoz@alsea.com.mx>
2020-04-18 23:45:03 -0500

    Agregados los links para los niveles

--- assets/scripts/folderName.js
+++ assets/scripts/folderName.js
@@ -1,1 +1,18 @@
-const folderName = null
+const folderName = location.pathname;
+
+document.querySelectorAll("#nivel").forEach(obj=>{
+  obj.textContent = firstUpper(folderName.split("/").reverse()[1]);
+})
+
+function firstUpper(cadena){
+    if(cadena ==='eespecial'){
+      return 'Especial';
+    }
+    
+    if(cadena === 'eadultos'){
+      return 'para Adultos';
+    }
+
+  return cadena
+  .replace(/^[a-z]{1}/g, function($1) { return $1.toUpperCase(); })
+}

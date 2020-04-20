58e087907c7f5d6d66e8501b62db30f33b1b9e0d
Carlos Muñoz <carlos.munoz@alsea.com.mx>
2020-04-19 20:49:58 -0500

    Agregado actividades de repaso

--- assets/scripts/despliegaActividades.js
+++ assets/scripts/despliegaActividades.js
@@ -1,5 +1,14 @@
 const nivelActual = location.search;
 
+const niveles={
+  "inicial":[ "lactantes", "maternales"],
+  "preescolar":["primero", "segundo","tercero"],
+  "primaria":["primero", "segundo", "tercero", "cuarto", "quinto", "sexto"],
+  "secundaria":["primero", "segundo","tercero"],
+  "eespecial":["preescolar", "primaria baja", "primaria alta", "especial secundaria", "especial laboral"],
+  "eadultos":["primaria", "secundaria incial", "secundaria avanzada"]
+};
+
 
 function getQueryParams(URL){
   let request = { };
@@ -14,9 +23,52 @@ function getQueryParams(URL){
     return request;
 }
 
+function firstUpper(cadena){
+    if(cadena ==='eespecial'){
+      return 'Especial';
+    }
+    
+    if(cadena === 'eadultos'){
+      return 'para Adultos';
+    }
+
+  return cadena
+  .replace(/^[a-z]{1}/g, function($1) { return $1.toUpperCase(); })
+}
 
 const r = getQueryParams(nivelActual);
 
 //document.querySelector("#nivel").textContent = r.nivel + " y la actividad es: " + r.actividad;
 document.querySelector("#link-nivel").href="../niveles/"+r.nivel+"/index.html";
 document.getElementById("img-link").src="/assets/images/btn/niveles/icono_"+r.nivel+"@2x.svg";
+
+window.addEventListener('load',()=>{
+  const nivelActual = location.search;
+  const r = getQueryParams(nivelActual);
+  //console.log(r);
+  const currentLevel = niveles[r.nivel];
+
+  const navbar = document.getElementById("navigation-pills");
+
+  currentLevel.forEach(l=>{
+    let btnNav = document.createElement('li')
+    let linkNav = document.createElement('a');
+    btnNav.role="presentation";
+    linkNav.href="#";
+    linkNav.innerText = firstUpper(l);
+    btnNav.appendChild(linkNav)
+    navbar.appendChild(btnNav);
+  })
+
+//  document.getElementById("contenido").innerHTML='<object type="text/html" data="links/link001.html" ></object>';
+  //aprende-en-casa-2\actividades\links\link001.html
+
+    fetch("links/link001.html" /*, options */)
+    .then((response) => response.text())
+    .then((html) => {
+        document.getElementById("contenido").innerHTML = html;
+    })
+    .catch((error) => {
+        console.warn(error);
+    });
+});

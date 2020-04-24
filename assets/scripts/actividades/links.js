5bfdaa15f74b3f7c6030fc2e59aca902747b0f44
deanvlue <munoz.josecarlos@gmail.com>
2020-04-23 22:39:36 -0500

    Se crean varias funciones para cargar el contenido de los grados y niveles

---
+++ assets/scripts/actividades/links.js
@@ -0,0 +1,58 @@
+const links ={
+    primaria:{
+        primero:{
+            tv:[
+                {
+                    name: 'Televisión educativa',
+                    link:'https://www.aprendeencasa.mx/aprende-en-casa/alumnos-secundaria/tv-educativa/index.html'
+                }
+            ],
+            fichas:[
+                {
+                    name: 'Google',
+                    link: 'https://google.com/'
+                },
+                {
+                    name: 'DuckDuckGo',
+                    link: 'https://duckduckgo.com/'
+                },
+                {
+                    name: 'Docker',
+                    link: 'https://docker.com/'
+                },
+                {
+                    name: 'Dotnet',
+                    link: 'https://docker.com/'
+                },
+                {
+                    name: 'Dragnet',
+                    link: 'https://docker.com/'
+                },
+            ],
+        },
+        segundo:{
+            tv:[
+                {
+                    name: 'Televisión educativa Segundo',
+                    link:'https://www.aprendeencasa.mx/aprende-en-casa/alumnos-secundaria/tv-educativa/index.html'
+                }
+            ],
+            fichas:[
+                {
+                    name: 'Google Segundo',
+                    link: 'https://google.com/'
+                },
+                {
+                    name: 'DuckDuckGo',
+                    link: 'https://duckduckgo.com/'
+                }
+            ],
+
+        },
+        tercero:{},
+        cuarto:{},
+        quinto:{},
+        sexto:{},
+    }
+        
+}
--- assets/scripts/despliegaActividades.js
+++ assets/scripts/despliegaActividades.js
@@ -1,5 +1,6 @@
 const nivelActual = location.search;
 let req;
+let grado = "primero";
 
 const niveles={
   "inicial":[ "lactantes", "maternales"],
@@ -39,7 +40,6 @@ function firstUpper(cadena){
 
 const r = getQueryParams(nivelActual);
 
-function openLink()
 
 //document.querySelector("#nivel").textContent = r.nivel + " y la actividad es: " + r.actividad;
 document.querySelector("#link-nivel").href="../niveles/"+r.nivel+"/index.html";
@@ -48,7 +48,32 @@ document.getElementById("img-link").src="/assets/images/btn/niveles/icono_"+r.ni
 window.addEventListener('load',()=>{
   const nivelActual = location.search;
   req = getQueryParams(nivelActual);
-  //console.log(r);
+  //console.log(req);
+  generaBotonesGrados(req);
+
+  cargaActividades(link);
+
+
+//  document.getElementById("contenido").innerHTML='<object type="text/html" data="links/link001.html" ></object>';
+  //aprende-en-casa-2\actividades\links\link001.html
+
+    /*fetch("links/mason.html")
+    .then((response) => response.text())
+    .then((html) => {
+        document.getElementById("contenido").innerHTML = html
+    })
+    .catch((error) => {
+        console.warn(error);
+    });*/
+    //console.log(links);
+    
+    // deprecated
+    //cambiaContenido(links, req.nivel, grado ,'fichas');
+});
+//  genera los botoones pills de grados
+function generaBotonesGrados(req){
+
+  // determina el nivel actual para deplegar los grados, ej. primaria: primero a sexto
   const currentLevel = niveles[req.nivel];
 
   const navbar = document.getElementById("navigation-pills");
@@ -58,20 +83,66 @@ window.addEventListener('load',()=>{
     let linkNav = document.createElement('a');
     btnNav.role="presentation";
     linkNav.href="#";
+    linkNav.id = l;
     linkNav.innerText = firstUpper(l);
     btnNav.appendChild(linkNav)
     navbar.appendChild(btnNav);
-  })
+  });
+}
 
-//  document.getElementById("contenido").innerHTML='<object type="text/html" data="links/link001.html" ></object>';
-  //aprende-en-casa-2\actividades\links\link001.html
+// lee los links y carga todos los botones
 
-    fetch("links/link001.html" /*, options */)
-    .then((response) => response.text())
-    .then((html) => {
-        document.getElementById("contenido").innerHTML = html;
+
+
+// esta funcion cambia el estado actual de la actividad seleccionada
+function setState(e, estado){
+  //document.getElementById(e.id).className = document.getElementById(e.id).className + " active";
+  cambiaContenido(links, req.nivel, grado ,e.id);
+}
+
+function cambiaContenido(linkDoc, nivel, grado , actividad){
+    let contenido = document.getElementById("contenido");
+    contenido.innerHTML="";
+    let links = {};
+    _links = linkDoc[nivel][grado][actividad];
+
+    _links.forEach(l=>{
+      let card = createCard(l, actividad);
+      contenido.appendChild(card);
     })
-    .catch((error) => {
-        console.warn(error);
-    });
-});
+    //contenido.innerHTML = '<p>' + JSON.stringify(links[nivel][actividad]) + '</p>';
+}
+
+function createCard(link, actividad){
+
+  let gridItem = document.createElement('div');
+    gridItem.className="grid-item";
+  let panel = document.createElement('div');
+    panel.className="thumbnail";
+  let image = document.createElement('img');
+      image.src='/assets/images/btn/actividades/min/icono-'+actividad+'.svg';
+      image.className="img-responsive"
+  let caption = document.createElement('div');  
+      caption.className="caption";
+  let anchor = document.createElement('a');
+        anchor.className="btn btn-primary"
+        anchor.role="button"
+        anchor.href=link.link
+        anchor.innerText="Leer";
+
+  let header = document.createElement('h3');
+    header.innerText = link.name;
+  let p = document.createElement('p');
+    p.appendChild(anchor);
+
+    // se constuye el caption
+    caption.appendChild(header);
+    caption.appendChild(p);
+    
+    // se construye el panel
+    panel.appendChild(image);
+    panel.appendChild(caption);
+
+    gridItem.appendChild(panel);
+  return gridItem;
+}  

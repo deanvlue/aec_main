7c8496aa8979d5cfe7aaf262e01d7a9568cca772
Carlos Muñoz <munoz.josecarlos@gmail.com>
2020-04-28 12:36:41 -0500

    Se remueven actividades estandard y tareas se ponen en blanco

--- assets/scripts/actividades/actividades.js
+++ assets/scripts/actividades/actividades.js
@@ -1,112 +1,121 @@
 /* jshint esversion: 6 */
 
 let agendaActividad = [
-    [
-        {
-            "tarea": "Selecciona tu actividad"
-        },
-        {
-            "tarea": "PAUSA ACTIVA"
-        },
-        {
-            "tarea": "Selecciona tu actividad"
-        },
-        {
-            "tarea": "Selecciona tu actividad"
-        }, {
-            "tarea": "PAUSA ACTIVA"
-        }, {
-            "tarea": "Selecciona tu actividad"
-        }
-    ],
-    [
-        {
-            "tarea": "Selecciona tu actividad"
-        },
-        {
-            "tarea": "PAUSA ACTIVA"
-        },
-        {
-            "tarea": "Selecciona tu actividad"
-        },
-        {
-            "tarea": "Selecciona tu actividad"
-        }, {
-            "tarea": "PAUSA ACTIVA"
-        }, {
-            "tarea": "Selecciona tu actividad"
-        }
-    ],
-    [
-        {
-            "tarea": "Selecciona tu actividad"
-        },
-        {
-            "tarea": "PAUSA ACTIVA"
-        },
-        {
-            "tarea": "Selecciona tu actividad"
-        },
-        {
-            "tarea": "Selecciona tu actividad"
-        }, {
-            "tarea": "PAUSA ACTIVA"
-        }, {
-            "tarea": "Selecciona tu actividad"
-        }
-    ],
-    [
-        {
-            "tarea": "Selecciona tu actividad"
-        },
-        {
-            "tarea": "PAUSA ACTIVA"
-        },
-        {
-            "tarea": "Selecciona tu actividad"
-        },
-        {
-            "tarea": "Selecciona tu actividad"
-        }, {
-            "tarea": "PAUSA ACTIVA"
-        }, {
-            "tarea": "Selecciona tu actividad"
-        }
-    ],
-    [
-        {
-            "tarea": "Selecciona tu actividad"
-        },
-        {
-            "tarea": "PAUSA ACTIVA"
-        },
-        {
-            "tarea": "Selecciona tu actividad"
-        },
-        {
-            "tarea": "Selecciona tu actividad"
-        }, {
-            "tarea": "PAUSA ACTIVA"
-        }, {
-            "tarea": "Selecciona tu actividad"
-        }
-    ]
+  [
+    {
+      tarea: "",
+    },
+    {
+      tarea: "PAUSA ACTIVA",
+    },
+    {
+      tarea: "",
+    },
+    {
+      tarea: "",
+    },
+    {
+      tarea: "PAUSA ACTIVA",
+    },
+    {
+      tarea: "",
+    },
+  ],
+  [
+    {
+      tarea: "",
+    },
+    {
+      tarea: "PAUSA ACTIVA",
+    },
+    {
+      tarea: "",
+    },
+    {
+      tarea: "",
+    },
+    {
+      tarea: "PAUSA ACTIVA",
+    },
+    {
+      tarea: "",
+    },
+  ],
+  [
+    {
+      tarea: "",
+    },
+    {
+      tarea: "PAUSA ACTIVA",
+    },
+    {
+      tarea: "",
+    },
+    {
+      tarea: "",
+    },
+    {
+      tarea: "PAUSA ACTIVA",
+    },
+    {
+      tarea: "",
+    },
+  ],
+  [
+    {
+      tarea: "",
+    },
+    {
+      tarea: "PAUSA ACTIVA",
+    },
+    {
+      tarea: "",
+    },
+    {
+      tarea: "",
+    },
+    {
+      tarea: "PAUSA ACTIVA",
+    },
+    {
+      tarea: "",
+    },
+  ],
+  [
+    {
+      tarea: "",
+    },
+    {
+      tarea: "PAUSA ACTIVA",
+    },
+    {
+      tarea: "",
+    },
+    {
+      tarea: "",
+    },
+    {
+      tarea: "PAUSA ACTIVA",
+    },
+    {
+      tarea: "",
+    },
+  ],
 ];
 
-
 const tareasDisponibles = [
-    "Ver la clase por TV",
-    "Actividad para Carpeta de experiencias ",
-    "Realizar ejercicios de la ficha de repaso",
-    "Revisión de libro de texto",
-    "Ver un video",
-    "Jugar un videojuego",
-    "Realizar una lectura ",
-    "Escuchar un audiolibro",
-    "Participar en un reto",
-    "Realizar una activación física",
-    "Reforzamiento del conocimiento ",
-    "Visitar un museo virtual ",
-    "Realizard Test Misión Paz Escolar",
-    "Ver una película de Cineclub familiar",
+  "Ver la clase por TV",
+  "Actividad para Carpeta de experiencias ",
+  "Realizar ejercicios de la ficha de repaso",
+  "Revisión de libro de texto",
+  "Ver un video",
+  "Jugar un videojuego",
+  "Realizar una lectura ",
+  "Escuchar un audiolibro",
+  "Participar en un reto",
+  "Realizar una activación física",
+  "Reforzamiento del conocimiento ",
+  "Visitar un museo virtual ",
+  "Realizard Test Misión Paz Escolar",
+  "Ver una película de Cineclub familiar",
 ];

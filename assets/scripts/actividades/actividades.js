3809bb79cfaf984bada29ac1126e6c2869caa1f3
Carlos Muñoz <munoz.josecarlos@gmail.com>
2020-04-28 11:23:51 -0500

    Funcionalidad e Agenda completa y Tooltip para cada botón de la agenda, adicionalmente para un usuario que no ha usado la agenda se le inicia la agenda vacía

--- assets/scripts/actividades/actividades.js
+++ assets/scripts/actividades/actividades.js
@@ -1,120 +1,112 @@
+/* jshint esversion: 6 */
+
 let agendaActividad = [
-  [
-    {
-      "tarea": "Lunes Algebra"
-    },
-    {
-      "tarea":"PAUSA ACTIVA"
-    },
-    {
-      "tarea": "Español Lunes"
-    },
-    {
-      "tarea": "Salud"
-    },
-    {
-      "tarea":"PAUSA ACTIVA"
-    },
-    {
-      "tarea": "Civismo"
-    }
-  ],
-  [
-    {
-      "tarea": "Martes Algebra"
-    },
-    {
-      "tarea":"PAUSA ACTIVA"
-    },
-    {
-      "tarea": "Español Martes"
-    },
-    {
-      "tarea": "Salud"
-    },
-    {
-      "tarea":"PAUSA ACTIVA"
-    },
-    {
-      "tarea": "Civismo"
-    }
-  ],
-  [
-    {
-      "tarea": "Algebra"
-    },
-    {
-      "tarea":"PAUSA ACTIVA"
-    },
-    {
-      "tarea": "Español Miércoles"
-    },
-    {
-      "tarea": "Salud"
-    },
-    {
-      "tarea":"PAUSA ACTIVA"
-    },
-    {
-      "tarea": "Civismo"
-    }
-  ],
-  [
-    {
-      "tarea": "Algebra"
-    },
-    {
-      "tarea":"PAUSA ACTIVA"
-    },
-    {
-      "tarea": ""
-    },
-    {
-      "tarea": "Salud"
-    },
-    {
-      "tarea":"PAUSA ACTIVA"
-    },
-    {
-      "tarea": "Civismo"
-    }
-  ],
-  [ 
-    {
-      "tarea": "Algebra"
-    },
-    {
-      "tarea":"PAUSA ACTIVA"
-    },
-    {
-      "tarea": "Español"
-    },
-    {
-      "tarea": "Salud"
-    },
-    {
-      "tarea":"PAUSA ACTIVA"
-    },
-    {
-      "tarea": ""
-    }
-  ]
+    [
+        {
+            "tarea": "Selecciona tu actividad"
+        },
+        {
+            "tarea": "PAUSA ACTIVA"
+        },
+        {
+            "tarea": "Selecciona tu actividad"
+        },
+        {
+            "tarea": "Selecciona tu actividad"
+        }, {
+            "tarea": "PAUSA ACTIVA"
+        }, {
+            "tarea": "Selecciona tu actividad"
+        }
+    ],
+    [
+        {
+            "tarea": "Selecciona tu actividad"
+        },
+        {
+            "tarea": "PAUSA ACTIVA"
+        },
+        {
+            "tarea": "Selecciona tu actividad"
+        },
+        {
+            "tarea": "Selecciona tu actividad"
+        }, {
+            "tarea": "PAUSA ACTIVA"
+        }, {
+            "tarea": "Selecciona tu actividad"
+        }
+    ],
+    [
+        {
+            "tarea": "Selecciona tu actividad"
+        },
+        {
+            "tarea": "PAUSA ACTIVA"
+        },
+        {
+            "tarea": "Selecciona tu actividad"
+        },
+        {
+            "tarea": "Selecciona tu actividad"
+        }, {
+            "tarea": "PAUSA ACTIVA"
+        }, {
+            "tarea": "Selecciona tu actividad"
+        }
+    ],
+    [
+        {
+            "tarea": "Selecciona tu actividad"
+        },
+        {
+            "tarea": "PAUSA ACTIVA"
+        },
+        {
+            "tarea": "Selecciona tu actividad"
+        },
+        {
+            "tarea": "Selecciona tu actividad"
+        }, {
+            "tarea": "PAUSA ACTIVA"
+        }, {
+            "tarea": "Selecciona tu actividad"
+        }
+    ],
+    [
+        {
+            "tarea": "Selecciona tu actividad"
+        },
+        {
+            "tarea": "PAUSA ACTIVA"
+        },
+        {
+            "tarea": "Selecciona tu actividad"
+        },
+        {
+            "tarea": "Selecciona tu actividad"
+        }, {
+            "tarea": "PAUSA ACTIVA"
+        }, {
+            "tarea": "Selecciona tu actividad"
+        }
+    ]
 ];
 
 
 const tareasDisponibles = [
-  "Ver la clase por TV",
-  "Actividad para Carpeta de experiencias ",
-  "Realizar ejercicios de la ficha de repaso",
-  "Revisión de libro de texto",
-  "Ver un video",
-  "Jugar un videojuego",
-  "Realizar una lectura ",
-  "Escuchar un audiolibro",
-  "Participar en un reto",
-  "Realizar una activación física",
-  "Reforzamiento del conocimiento ",
-  "Visitar un museo virtual ",
-  "Realizard Test Misión Paz Escolar",
-  "Ver una película de Cineclub familiar",
+    "Ver la clase por TV",
+    "Actividad para Carpeta de experiencias ",
+    "Realizar ejercicios de la ficha de repaso",
+    "Revisión de libro de texto",
+    "Ver un video",
+    "Jugar un videojuego",
+    "Realizar una lectura ",
+    "Escuchar un audiolibro",
+    "Participar en un reto",
+    "Realizar una activación física",
+    "Reforzamiento del conocimiento ",
+    "Visitar un museo virtual ",
+    "Realizard Test Misión Paz Escolar",
+    "Ver una película de Cineclub familiar",
 ];

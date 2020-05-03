73779ac55531a0e75d41908220e8534d3f40178c
Carlos Muñoz <munoz.josecarlos@gmail.com>
2020-05-03 14:55:43 -0500

    Agregado del titulo para videos

--- assets/scripts/videos_inicio/videosDisplay.js
+++ assets/scripts/videos_inicio/videosDisplay.js
@@ -11,29 +11,29 @@ new Vue({
         },
         {
           id:"2",
-          titulo: 'Un millón de estrellas',
-          descripcion: 'Distingue distintas actitudes asertivas que se manifiestan en las actividades y juegos para promover ambientes de colaboración y respeto.',
-          url:'https://www.aprendeencasa.mx/aprende-en-casa/videos/IXTZULsensibilizacion.mp4'
-        },
-        {
-          id:"3",
           titulo: 'Tamandua',
           descripcion: 'Buscar varias soluciones para la resolución de problemas y echar a andar la creatividad de cada uno. Intercambiar opiniones con sus familiares y hermanos para establecer acuerdos que beneficien el trabajo',
           url:'https://res.cloudinary.com/dd1cjovcw/video/upload/v1588525831/videos/tamandua_min_nz0iry.mp4'
         },
         {
-          id:"4",
+          id:"3",
           titulo: 'Kokone',
           descripcion: 'Promover y contribuir en los alumnos el desarrollo físico,           intelectual, social y afectivo  en un marco familiar y de convivencia sana a través de la resolución de tareas y problemas motrices con carácter lúdico estimulando el desarrollo de sus habilidades y destrezas de  forma global en forma lúdica.',
           url: 'https://res.cloudinary.com/dd1cjovcw/video/upload/v1588525812/videos/kokone_circuito_min_fkppel.mp4'
         },
         {
-          id:"5",
+          id:"4",
           titulo: 'Teporingos',
           descripcion: 'La actividad de búsqueda del tesoro, está dirigida a un nivel preescolar por medio de especialistas en campismo; la cual hace una actividad de interés para el educando y su familia en esta temporalidad de pandemia, la invitación se proyecta por medio de un video de cómo realizar actividades dentro de casa sin tener que salir de ella, teniendo todos los recursos para su ejecución.',
           url: 'https://www.aprendeencasa.mx/aprende-en-casa/videos/TEPORINGOSbusqueda_tesoro.mp4'
         },
         
+        {
+          id:"5",
+          titulo: 'Un millón de estrellas',
+          descripcion: 'Distingue distintas actitudes asertivas que se manifiestan en las actividades y juegos para promover ambientes de colaboración y respeto.',
+          url:'https://www.aprendeencasa.mx/aprende-en-casa/videos/IXTZULsensibilizacion.mp4'
+        },
       ]
   }
 });

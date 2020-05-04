26ae7e0ca0db2cfeb01b655304001ec5f01d86dc
Carlos Muñoz <munoz.josecarlos@gmail.com>
2020-05-03 23:41:56 -0500

    Cambios en formado de videos

--- assets/scripts/videos_inicio/videosDisplay.js
+++ assets/scripts/videos_inicio/videosDisplay.js
@@ -5,39 +5,39 @@ new Vue({
      videoLinks: [
         {
           id:"1",
-          titulo: 'Danza, juega y gana',
-          url: 'https://res.cloudinary.com/dd1cjovcw/video/upload/v1588561666/videos/1_danza_ritmo_jkdxxo.mov'
+          titulo: 'Un millón de estrellas',
+          url:'https://www.aprendeencasa.mx/aprende-en-casa/videos/IXTZULsensibilizacion.mp4'
         },
         {
           id:"2",
-          titulo: 'Cuerdas divertidas',
-          url: 'https://res.cloudinary.com/dd1cjovcw/video/upload/v1588561814/videos/2_danza_cuerda_huiz9g.mov'
-        },
-        {
-          id:"3",
           titulo: 'Un teatro ¿de sombras?',
           url: 'https://res.cloudinary.com/dd1cjovcw/video/upload/v1588525806/videos/libertad_min_lvf2mo.mp4'
         },
         {
-          id:"5",
+          id:"3",
           titulo: 'Apaches en acción',
           url: 'https://res.cloudinary.com/dd1cjovcw/video/upload/v1588525812/videos/kokone_circuito_min_fkppel.mp4'
         },
         {
-          id:"6",
-          titulo: 'La busqueda del tesoro',
+          id:"4",
+          titulo: 'La búsqueda del tesoro',
           url: 'https://www.aprendeencasa.mx/aprende-en-casa/videos/TEPORINGOSbusqueda_tesoro.mp4'
         },
         {
-          id:"4",
+          id:"5",
           titulo: '¡Chic@s en acción!',
           url:'https://res.cloudinary.com/dd1cjovcw/video/upload/v1588525831/videos/tamandua_min_nz0iry.mp4'
         },
         
         {
+          id:"6",
+          titulo: 'Danza, juega y gana',
+          url: 'https://res.cloudinary.com/dd1cjovcw/video/upload/v1588561666/videos/1_danza_ritmo_jkdxxo.mov'
+        },
+        {
           id:"7",
-          titulo: 'Un millón de estrellas',
-          url:'https://www.aprendeencasa.mx/aprende-en-casa/videos/IXTZULsensibilizacion.mp4'
+          titulo: 'Cuerdas divertidas',
+          url: 'https://res.cloudinary.com/dd1cjovcw/video/upload/v1588561814/videos/2_danza_cuerda_huiz9g.mov'
         },
       ]
   }

e38a82df7756004dcabcd6a48be052971b4e752b
Carlos Muñoz <munoz.josecarlos@gmail.com>
2020-07-24 23:21:18 -0500

    se agregan audios de airelibre

---
+++ assets/scripts/videos_inicio/audiosInformativos.js
@@ -0,0 +1,88 @@
+const aLinks = [
+    {
+        id: "1",
+        videoId: "MHmThDADYus"
+    },
+    {
+        id: "2",
+        videoId: "B5ZAJZtPaWs"
+    },
+
+    {
+
+        id: "3",
+        videoId: "9xwPIgZ-osQ"
+    }
+    ,
+    {
+
+        id: "4",
+        videoId: "zfwy_uYtm5g"
+
+    },
+    {
+
+        id: "5",
+        videoId: "HT9XAoduwMc"
+
+    },
+    {
+
+        id: "6",
+        videoId: "k18AOWNlrYg"
+
+    },
+    {
+        id: "7",
+        videoId: "S6r0njg_hHQ"
+
+    },
+    {
+        id: "8",
+
+        videoId: "adA8B-4RHrk"
+
+    },
+    {
+        id: "9",
+        videoId: "uYtseuxZxMY"
+
+    },
+    {
+
+        id: "10",
+        videoId: "RnB9a2xtjE4"
+
+    },
+    {
+
+        id: "11",
+        videoId: "xC8ImoTckVQ"
+    }
+    ,
+    {
+
+        id: "12",
+        videoId: "YmITBgXXZBo"
+
+    },
+    {
+
+        id: "13",
+        videoId: "B-Qc7bz7iWE"
+
+    },
+    {
+
+        id: "14",
+        videoId: "pU44SUHPcW8"
+    }
+];
+
+new Vue({
+    el: "#audiosInformativos",
+    data: {
+        audioLinks: aLinks
+    }
+});
+
